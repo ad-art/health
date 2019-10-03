11c2798e126b84dfa71aa61c39f6e1703a236c9d
ad_art <ad.ar@yandex.ru>
2019-10-03 17:14:28 +0500

    with xml config

--- src/main/java/org/health/controller/AppController.java
+++ src/main/java/org/health/controller/AppController.java
@@ -1,17 +1,33 @@
 package org.health.controller;
 
+import org.health.model.Cat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@ImportResource(value = "classpath:ioc.xml")
 public class AppController {
+
+    // http://localhost:8080/
     @RequestMapping("/hello/{name}")
 
     public String getHelloPage(@PathVariable("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello";
 
+    }
+
+    @Autowired
+    private Cat cat;
+
+    @RequestMapping("/")
+    public String getName(Model model) {
+        model.addAttribute("name", cat.getName());
+        return "cat";
     }
 }
---
+++ src/main/java/org/health/model/Cat.java
@@ -0,0 +1,22 @@
+package org.health.model;
+
+public class Cat {
+
+    public Cat() {
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
