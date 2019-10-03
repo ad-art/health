4e21f0451660fe2161f5a3575cb0cbe539ddc173
ad_art <ad.art@yandex.ru>
2019-10-03 22:28:22 +0500

    using @Primary

--- src/main/java/org/health/model/impl/Turtle.java
+++ src/main/java/org/health/model/impl/Turtle.java
@@ -1,11 +1,15 @@
 package org.health.model.impl;
 
 import org.health.model.Animal;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
 
+@Service
+@Primary
 public class Turtle implements Animal {
 
     @Override
     public String getInfo() {
-        return null;
+        return "Don";
     }
 }
