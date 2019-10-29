6c63e615df83004891546a592e2af205f1ddf177
ad_art <ad.ar@yandex.ru>
2019-10-29 14:47:51 +0500

    testAddAndGet

--- src/test/java/org/health/controller/HorseControllerIntegrationTest.java
+++ src/test/java/org/health/controller/HorseControllerIntegrationTest.java
@@ -2,12 +2,15 @@ package org.health.controller;
 
 import org.health.entity.Horse;
 import org.health.entity.Mare;
+import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 
 public class HorseControllerIntegrationTest {
@@ -21,12 +24,38 @@ public class HorseControllerIntegrationTest {
     @Test
     public void testAddAndGet() {
         Horse rysak = createHorse();
+
+        RestTemplate template= new RestTemplate();
+        ResponseEntity<Horse> responseEntity = template.exchange(
+                ROOT + GET + "/{id}",
+                HttpMethod.GET,
+                null,  // request body
+                Horse.class,  // class to cast
+                rysak.getId()
+        );
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode()); // status code == 200
+        Horse receivedHorse =responseEntity.getBody();
+        assertNotNull(receivedHorse);
     }
 
     private Horse createHorse() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
         Horse horse = prefillHorse();
+
+        HttpEntity<Horse> entity = new HttpEntity<>(horse, headers);
+
+        RestTemplate template = new RestTemplate();
+        Horse createdHorse = template.exchange(
+                ROOT +ADD,
+                HttpMethod.POST,
+                entity,
+                Horse.class
+        ).getBody();
+        assertNotNull(createdHorse);
+        assertEquals("Rysak", createdHorse.getName());
+        return createdHorse;
     }
 
     private Horse prefillHorse() {
@@ -40,8 +69,8 @@ public class HorseControllerIntegrationTest {
 
 
         Mare mare2 = new Mare();
-        mare1.setName("Porridge");
-        mare1.setDescription("Tasty");
+        mare2.setName("Porridge");
+        mare2.setDescription("Tasty");
 
         List<Mare> list = new ArrayList<>();
         list.add(mare1);
